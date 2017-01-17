a1ed1142c4faf6ed9f731297b5aed05701df4b9f
杨植生 <vincent.yang@jinfuzi.com>
2017-01-17 09:43:55 +0800

    Initial Commit

---
+++ LuanchDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LuanchDemo
+//
+//  Created by vincent on 2017/1/17.
+//  Copyright © 2017年 FuTouBang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
